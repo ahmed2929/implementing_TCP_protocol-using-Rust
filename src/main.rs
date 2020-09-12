efcd75767effef76dcb0229afde315cdccf6d67e
ahmed2929 <mywork304@gmail.com>
2020-09-12 21:14:53 +0200

    fix buffer max

--- src/main.rs
+++ src/main.rs
@@ -2,7 +2,7 @@ use std::io;
 
  fn main()->io::Result<()> {
  let nic=tun_tap::Iface::new("tun0", tun_tap::Mode::Tun)?;
- let mut buffer=[0u8,150];
+ let mut buffer = [0; 512];
  let Number_of_bytes=nic.recv(&mut buffer[..])?;
  eprintln!("read {} bytes {:x?}",Number_of_bytes,&buffer[..Number_of_bytes]);
  Ok(())
